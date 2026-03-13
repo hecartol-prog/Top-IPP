24e9ee64566df8537493190f275970aec71b0931
hector <hector@dgtopindustrial.com>
2026-03-13 13:54:28 +0000

    File changes

--- src/App.jsx
+++ src/App.jsx
@@ -59,6 +59,7 @@ const AuthenticatedApp = () => {
           }
         />
       ))}
+      <Route path="/Tasks" element={<LayoutWrapper currentPageName="Tasks"><Tasks /></LayoutWrapper>} />
       <Route path="*" element={<PageNotFound />} />
     </Routes>
   );
