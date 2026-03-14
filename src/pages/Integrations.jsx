818a489aa9db29dab32651f232858ad70c726391
hector <hector@dgtopindustrial.com>
2026-03-14 10:33:56 +0000

    File changes

--- src/pages/Integrations.jsx
+++ src/pages/Integrations.jsx
@@ -39,6 +39,15 @@ export default function Integrations() {
           >
             <LinkedInPanel />
           </motion.div>
+
+          {/* HubSpot Integration */}
+          <motion.div
+            initial={{ opacity: 0, y: 20 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ delay: 0.3 }}
+          >
+            <HubSpotPanel />
+          </motion.div>
         </div>
 
         {/* Usage Tips */}
